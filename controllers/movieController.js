2b54c8c871b9219027d362c4e1a85e2426d3c50f
Olufunto Oyewole <funti009@gmail.com>
2022-05-18 11:18:57 +0100

    created the review controller and setup the environmental variables

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,8 +1,81 @@
-function getAllMovies(req,res){
-    res.send('All Movies')
+const movies_db= require('../database/movies/movies');
+const axios = require('axios').default;
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+//Method GET
+//Path /:id
+//Description Getting all the movies from the database
+function getMovieById(req,res){
+    const {id}= req.params
+    
+    
+    //picks out the movie id that matches the parameter
+    const searchedMovie= movies_db.movies.filter((movie)=>{
+        return movie.id===Number(id)
+    })
+    
+    console.log(searchedMovie)
+    res.render('movieCharles',{searchedMovie,allGenres:movies_db.genres})
+//    res.send(`id :${id}`)
 }
 
 
+//Method GET
+//Path /search
+//Description Getting all the movies from the database
+function searchMovies(req,res){
+    const {query}= req;
+    let searchedMovie;
+    
+    //this runs when the query path is "genre"
+    if(query.genre){
+       const genreName=query.genre;
+        
+        searchedMovie=movies_db.movies.filter((movie)=>{
+            let convertedText=[]
+            
+            //changes the genre strings to lowercase
+            movie.genres.forEach((item)=>{
+               convertedText.push(item.toLowerCase()) 
+            })
+            
+            return convertedText.includes(genreName)
+        })
+        
+       
+    }
+//    img(src=movie.posterUrl height=300 width=200)
+    res.render('movieCharles', {searchedMovie,allGenres:movies_db.genres})
+//    res.send('searching')
+}
+
+async function checkReviews(req,res){
+    const {query}= req;
+    const reveiwedMovie=query.search;
+//    reveiwedMovie=reveiwedMovie.replace(" ", "%20")
+    const apiKey= process.env.MOVIE_KEY
+    
+    const url=`https://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&offset=3&order=by-publication-date&query=${reveiwedMovie}&api-key=${apiKey}`;
+    
+    try{
+       const response= await axios.get(url);
+        const data= response.data.results || []
+        
+        res.render('reviewsCharles',{data})
+    }catch(error){
+        console.log(error)
+        res.send('not working')
+    }
+    
+    
+    
+    
+}
+
 module.exports={
-    getAllMovies,
+    getMovieById,
+    searchMovies,
+    checkReviews
 }
--- routes/movie.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const router= express.Router();
-const {getAllMovies} = require(../controllers/movieController)
-
-
-router.get('/',getAllMovies)
-
-
-module.exports=router
